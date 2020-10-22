b12c214daf14dff3485a21ae95e9ff4bfafb3c7f
Yasunari t <u7df25c588m737@gmail.com>
2020-10-22 22:32:33 +0900

    ✨feat: build plugins/GoogleTranslate with GoogleAppScript

--- App.tsx
+++ App.tsx
@@ -8,20 +8,33 @@ import useCachedResources from './src/hooks/useCachedResources'
 import Navigation from './src/navigation'
 // Store
 import store from './src/store'
+// Translate Test
+import Translate from './src/plugins/GoogleTranslate'
 
 export default function App() {
-  const isLoadingComplete = useCachedResources()
+  Translate('i should go to home', 'en', 'ja')
+  Translate('私は家に帰るべきです', 'ja', 'en')
+  return (
+    // ノッチとかの処理をしてくれる
+    <SafeAreaProvider>
+      <Provider store={store}>
+        <Navigation />
+      </Provider>
+      <StatusBar />
+    </SafeAreaProvider>
+  )
+  // const isLoadingComplete = useCachedResources()
 
-  if (isLoadingComplete) {
-    return (
-      // ノッチとかの処理をしてくれる
-      <SafeAreaProvider>
-        <Provider store={store}>
-          <Navigation />
-        </Provider>
-        <StatusBar />
-      </SafeAreaProvider>
-    )
-  }
+  // if (isLoadingComplete) {
+  //   return (
+  //     // ノッチとかの処理をしてくれる
+  //     <SafeAreaProvider>
+  //       <Provider store={store}>
+  //         <Navigation />
+  //       </Provider>
+  //       <StatusBar />
+  //     </SafeAreaProvider>
+  //   )
+  // }
   return null
 }
--- src/plugins/GoogleTranslate.ts
+++ src/plugins/GoogleTranslate.ts
@@ -1,3 +1,23 @@
-const GoogleTranslate = () => {}
+import axios from 'axios'
+import { GOOGLE_APP_SCRIPT_URL } from '@env'
+
+// Note: fetch だと xss 的なエラーが出る
+const GoogleTranslate = async (text: string, source: string, target: string): Promise<string> => {
+  console.log('--- GoogleTranslate ---')
+
+  const translatedText = await axios(GOOGLE_APP_SCRIPT_URL, {
+    params: {
+      text,
+      source,
+      target,
+    },
+  }).then((res) => {
+    return res.data
+  })
+
+  console.log(translatedText)
+
+  return translatedText
+}
 
 export default GoogleTranslate
